b108f83da9b13f2e386c26515373f3feac68c8e5
yarocruz <yarocruz@gmail.com>
2023-04-19 14:16:13 -0400

    change url for fetch requests

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,7 @@ function Login() {
         const user = { email, password };
 
         // Send login request to server
-        const response = await fetch('http://localhost:8080/login', {
+        const response = await fetch('https://neon-marzipan-917d6a.netlify.app/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -79,7 +79,7 @@ function App() {
     const [tasks, setTasks] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:8080/tasks', {
+        fetch('https://neon-marzipan-917d6a.netlify.app/tasks', {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + localStorage.getItem('token')
