b3360bc542fe5ae878fcdfaaa27eca3354874577
yarocruz <yarocruz@gmail.com>
2023-04-19 21:43:37 -0400

    test auto deploy to heroku

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const data = {
 
 app.use(morgan("dev"));
 app.use(express.json());
-//app.use(cors());
+//app.use(cors()); // allow all origins
 
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
