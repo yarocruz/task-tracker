cd9204fff49a2f86d5c35374912881214a323232
yarocruz <yarocruz@gmail.com>
2023-04-20 11:04:14 -0400

    add no-cors again

--- client/src/App.js
+++ client/src/App.js
@@ -27,6 +27,7 @@ function Login() {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                'cors': 'no-cors',
             },
             body: JSON.stringify(user),
         });
@@ -70,6 +71,7 @@ function App() {
         fetch('https://immense-citadel-53026.herokuapp.com/tasks', {
             headers: {
                 'Authorization': 'Bearer ' + localStorage.getItem('token'),
+                'cors': 'no-cors',
             }
         })
             .then(response => response.json())
