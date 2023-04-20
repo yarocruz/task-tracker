c32e2da15d9d090dbe1b41b68ba315872124935f
yarocruz <yarocruz@gmail.com>
2023-04-19 21:31:01 -0400

    testing fetch to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ function Login() {
         const user = { email, password };
 
         // Send login request to server
-        const response = await fetch('http://localhost:8080/login', {
+        const response = await fetch('https://immense-citadel-53026.herokuapp.com/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -67,7 +67,7 @@ function App() {
     const [tasks, setTasks] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:8080/tasks', {
+        fetch('https://immense-citadel-53026.herokuapp.com/tasks', {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + localStorage.getItem('token')
